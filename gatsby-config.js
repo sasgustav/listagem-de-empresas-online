63e54cff8ec5ba563772073f8c7463aa6a10b172
Gustavo Vasconcelos <gustavovasconcelos.sc@gmail.com>
2024-05-20 22:47:30 -0300

    ghj

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,57 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby + Vercel`,
-    siteUrl: `https://gatsby-template.vercel.app/`,
+    title: `Seu Site Incrível`, 
+    description: `A melhor descrição do seu site`, 
+    twitterUsername: `@seu_twitter`, 
+    image: `/icon.png`,
+    siteUrl: `https://www.seusite.com`, 
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        host: `https://www.seusite.com`,
+        sitemap: `https://www.seusite.com/sitemap.xml`,
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby + Vercel`,
+        short_name: `Gatsby + Vercel`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID"
+      },
+    },
+    `gatsby-plugin-mdx`,
+  ],
 };
---
+++ src/hooks/use-site-metadata.js
@@ -0,0 +1,18 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+export const useSiteMetadata = () => {
+  const data = useStaticQuery(graphql`
+    query SiteMetadataQuery {
+      site {
+        siteMetadata {
+          title
+          description
+          twitterUsername
+          image
+          siteUrl
+        }
+      }
+    }
+  `);
+  return data.site.siteMetadata;
+};
